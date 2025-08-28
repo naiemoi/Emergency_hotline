8ef61b57f91777cafe1b470ded7b941322e8a972
Md.Naiem <naiem.mbstu.ict@gmail.com>
2025-08-28 20:45:49 +0600

    DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// DOM elements
+const lifeCount = document.querySelector('.flex.items-center.justify-center:nth-child(1) p');
+const coinCount = document.querySelector('.flex.items-center.justify-center:nth-child(2) p');
+const copyCount = document.querySelector('.flex.items-center.justify-center:nth-child(3) p');
+const historyList = document.querySelector('.history-list');
+const noCalls = document.getElementById('no-calls');
+const clearBtn = document.getElementById('btn-clear');
+const heartButtons = document.querySelectorAll('.btn-heart');
+const copyButtons = document.querySelectorAll('.bg-gray-200.text-gray-700');
+const callButtons = document.querySelectorAll('.btn-call');
